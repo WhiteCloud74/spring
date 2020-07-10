b3d00eca5ab4711e32b655f16aeac8cd1f7d82c1
WhiteCloud74 <15919947756@139.com>
2020-07-10 10:10:00 +0800

    完成客户端及服务端更新配置

--- spring-cloud-config-server/src/main/kotlin/com/example/springcloudconfigserver/SpringCloudConfigServerApplication.kt
+++ spring-cloud-config-server/src/main/kotlin/com/example/springcloudconfigserver/SpringCloudConfigServerApplication.kt
@@ -3,11 +3,13 @@ package com.example.springcloudconfigserver
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.config.server.EnableConfigServer
+import org.springframework.cloud.context.config.annotation.RefreshScope
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication
 @EnableConfigServer
 @EnableEurekaClient
+@RefreshScope
 class SpringCloudConfigServerApplication
 
 fun main(args: Array<String>) {
